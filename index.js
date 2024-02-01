a3e18ad22fc010a0f4dfa1c1fa5c097131a68a26
silvanna-van-meel <142269144+silvanna-van-meel@users.noreply.github.com>
2024-02-01 12:10:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// JavaScript-code toevoegen
 $(document).ready(function () {
-    // de knop selecteren
     var knop = $("#button");
-    // de bericht selecteren
     var bericht = $("#message");
-    // een klik-event toevoegen aan de knop
     knop.click(function () {
-        // de tekst van de bericht veranderen
-        bericht.text("Hallo, dit is een simpele jQuery-toepassing!");
+        bericht.text("Hallo Amer, bedankt voor uw begeleiding tijdens dit traject!");
     });
 });
